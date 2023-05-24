f83457e6adfd19c1bbee53e5b3012ccc8826f853
thantzinswe <thantzinswe01@gmail.com>
2023-05-24 23:04:35 +0630

    game develop pairs and click

--- src/App.js
+++ src/App.js
@@ -2,17 +2,48 @@ import { useState } from "react";
 import "./App.css";
 import Images from "./Images";
 import { shuffle } from "lodash";
+import { click } from "@testing-library/user-event/dist/click";
 
 function App() {
   const [cards, setCards] = useState(shuffle([...Images, ...Images]));
   const [activeCards, setActiveCards] = useState([]);
+  const [matchCards, setMatchCards] = useState([]);
+  const [won, setWon] = useState(false);
+  const [click, setClick] = useState(0);
 
   const showCard = (index) => {
-    setActiveCards([index]);
+    setClick((prev) => prev + 1);
+
+    if (activeCards.length === 0 || activeCards.length === 2) {
+      setActiveCards([index]);
+    }
+
+    if (activeCards.length === 1) {
+      const firstIndex = activeCards[0];
+      const secondIndex = index;
+
+      if (
+        cards[firstIndex] === cards[secondIndex] &&
+        firstIndex !== secondIndex
+      ) {
+        setMatchCards([...matchCards, firstIndex, secondIndex]);
+
+        if (matchCards.length + 2 === cards.length) {
+          setWon(true);
+        }
+      }
+
+      setActiveCards([...activeCards, index]);
+    }
   };
 
   return (
     <div>
+      {won && (
+        <p className="game-won">
+          Congratulations! You won the game. Click count is {click} clicks.
+        </p>
+      )}
       <div className="board">
         {cards.map((card, index) => {
           return (
@@ -20,7 +51,10 @@ function App() {
               key={index}
               className={
                 "parent-card " +
-                (activeCards.indexOf(index) !== -1 ? "show" : "")
+                (activeCards.indexOf(index) !== -1 ||
+                matchCards.indexOf(index) !== -1
+                  ? "show"
+                  : "")
               }
               onClick={() => showCard(index)}
             >
@@ -33,6 +67,9 @@ function App() {
             </div>
           );
         })}
+      </div>
+      <div className="game-info">
+        Click : {click} &nbsp;&nbsp;&nbsp;&nbsp; Pairs : {matchCards.length / 2}
       </div>
     </div>
   );
